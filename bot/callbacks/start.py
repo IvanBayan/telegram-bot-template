bdafbaeb36ccac9f29ca7fdd5e0b89ec1babbdbc
Illia <yarfuo@protonmail.com>
2019-07-09 00:13:30 +0300

    Add user to database after `/start`

--- bot/callbacks/start.py
+++ bot/callbacks/start.py
@@ -1,7 +1,24 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+import bot
+from bot import model
 
+
+# Imaginary attacker can start bot with something other than `/start` via
+#  MTProto and raise exception somewhere(interaction with db), but who cares?
 def handle_start(update: Update, context: CallbackContext):
     del context  # Not used
     update.effective_chat.send_message("Hello world!")
+    session = bot.Session()
+    user = (
+        session.query(model.User)
+        .filter_by(ext_user_id=update.effective_user.id)
+        .first()
+    )
+    if not user:
+        user = model.User(ext_user_id=update.effective_user.id)
+        session.add(user)
+        session.commit()
+
+    bot.Session.remove()
