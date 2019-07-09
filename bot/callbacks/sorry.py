2aefa692549217ae7153402f3d8901ebbf6768df
Illia <yarfuo@protonmail.com>
2019-07-09 20:05:28 +0300

    Sorry callback fix

--- bot/callbacks/sorry.py
+++ bot/callbacks/sorry.py
@@ -4,6 +4,6 @@ from telegram.ext import CallbackContext
 
 def handle_unknown(update: Update, context: CallbackContext):
     del context  # Not used
-    update.effective_chat.reply_text(
+    update.effective_message.reply_text(
         "Sorry, I do not understand you. Try /start command"
     )
