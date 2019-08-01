363125826f85c6bca177675be02d7d34ddf0bdc0
Illia <yarfuo@protonmail.com>
2019-08-01 08:50:40 +0300

    Little import fix

--- bot/callbacks/__init__.py
+++ bot/callbacks/__init__.py
@@ -1,6 +1,6 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters
 
-from bot.callbacks import sorry, start
+from callbacks import sorry, start
 
 HANDLERS = [
     CommandHandler("start", start.handle_start),
